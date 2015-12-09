a3c4b262eb24e4bff9b4309d99fb281e86abed32
root <root@localhost.localdomain>
2015-12-09 17:39:02 +0000

    syusei

--- contest/kaibun.php
+++ contest/kaibun.php
@@ -1,25 +1,31 @@
 <?php
 
-$roop_count = 1;
+$roop_count = 0;
+$kaibun_count = 0;
 
-for ($keta=1; $keta <= $argv[1] ; $keta++) {
-	echo '['.$roop_count++.']' . $keta . '桁' . PHP_EOL;
+// 1桁は回文に含めないので２桁から指定桁までループする
+for ($keta=2; $keta <= $argv[1] ; $keta++) {
+	$roop_count++;
 
-	if($keta == 1 || $keta % 2 == 0){
+	if($keta % 2 == 0){
 		display(0, $keta - 1, sprintf("%0" . $keta . "d", "0"));
 	} else {
-		for ($kisu=0; $kisu < 9; $kisu++) {
+		for ($kisu=0; $kisu <= 9; $kisu++) {
+			$roop_count++;
 			$base_num = sprintf("%0" . $keta . "d", "0");
-			$base_num = substr_replace($base_num, $kisu, floor($keta / 2) + 1, 1);
-			echo $base_num; exit;
-			display(0, $keta - 1, );
+			$base_num = substr_replace($base_num, $kisu, floor($keta / 2), 1);
+
+			display(0, $keta - 1, $base_num);
 		}
 	}
 }
 
+echo '回文個数：' . $kaibun_count . PHP_EOL;
+echo 'ループ回数：' . $roop_count . PHP_EOL;
 
 function display($first, $second, $num){
 	global $roop_count;
+	global $kaibun_count;
 
 	for($i = 1; $i <= 9; $i++){
 
@@ -31,12 +37,14 @@ function display($first, $second, $num){
 		}
 
 		$display_num = $num + intval($i . $second_num);
-		echo '[' . $roop_count . ']' . $display_num . PHP_EOL;
-		$roop_count++;
+		echo $display_num . PHP_EOL;
 
 		if($second - $first > 2){
-
 			display($first + 1, $second -1, $display_num);
 		}
+
+
+		$kaibun_count++;
+		$roop_count++;
 	}
 }
